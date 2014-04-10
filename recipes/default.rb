a6e794e45b4f2847c6dc5a42f363514ba921afd9
Yusuke Tanaka <y.tanaka@aktsk.jp>
2014-04-10 11:48:03 +0900

    Add remove_mysql_repository execution

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 rpm_path = node['mysql_community_release']['rpm_path']
 
+execute "remove_mysql_repository" do
+  command "yum remove -y 'mysql*'"
+  only_if "rpm -qa | grep mysql | grep -v mysql-community"
+end
+
 execute "add_mysql_community_repository" do
   command "yum install -y http://repo.mysql.com/#{rpm_path}"
   not_if "rpm -qa | grep -q mysql-community-release"
